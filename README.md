4805ce3c9ca0d370a507cc8dce9ca075c28ee203
dholdaway <d.m.holdaway@googlemail.com>
2016-03-04 11:44:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# RESTful API in Node and Express
+
+Using the new Express 4.0 Router to build an API
+
+## Requirements
+
+- Node and npm
+
+## Installation
+
+- Clone the repo: `git clone git@github.com:scotch-io/node-api`
+- Install dependencies: `npm install`
+- Start the server: `node server.js`
+
+## Testing the API
+
+Route   HTTP Verb   Description
+/api/users  GET Get all the users.
+/api/users  POST    Create a user.
+/api/users/:user_id GET Get a single user.
+/api/users/:user_id PUT Update a user with new info.
+/api/users/:user_id DELETE  Delete a user.
+
+#coming soon
+
+Token Auth
---
+++ server.js
@@ -0,0 +1,123 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+var morgan     = require('morgan');
+
+// configure app
+app.use(morgan('dev')); // log requests to the console
+
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port     = process.env.PORT || 8080; // set our port
+
+var mongoose   = require('mongoose');
+require('dotenv').config();
+//mongoose.connect('mongodb://process.env.DB_USER:process.env.DB_PASS@process.env.DB_HOST');
+mongoose.connect('mongodb://process.env.DB_USER:process.env.DB_PASS@ds019498.mlab.com:19498/users'); // connect to our database
+var User     = require('./app/models/user');
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'Yo! welcome to our api!' });	
+});
+
+// on routes that end in /users
+// ----------------------------------------------------
+router.route('/users')
+
+	// create a user (accessed at POST http://localhost:8080/users)
+	.post(function(req, res) {
+		
+		var user = new User();		// create a new instance of the User model
+		user.name = req.body.name;  // set the users name (comes from the request)
+
+		user.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'User created!' });
+		});
+
+		
+	})
+
+	// get all the users (accessed at GET http://localhost:8080/api/users)
+	.get(function(req, res) {
+		User.find(function(err, users) {
+			if (err)
+				res.send(err);
+
+			res.json(users);
+		});
+	});
+
+// on routes that end in /users/:user_id
+// ----------------------------------------------------
+router.route('/users/:user_id')
+
+	// get the user with that id
+	.get(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+			if (err)
+				res.send(err);
+			res.json(user);
+		});
+	})
+
+	// update the user with this id
+	.put(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+
+			if (err)
+				res.send(err);
+
+			user.name = req.body.name;
+			user.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'User updated!' });
+			});
+
+		});
+	})
+
+	// delete the user with this id
+	.delete(function(req, res) {
+		User.remove({
+			_id: req.params.user_id
+		}, function(err, user) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
